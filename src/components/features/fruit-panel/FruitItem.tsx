5415519b0b8cc65b66370c1e5a7eec1a2980343d
Kris Hokr <kristoferhokr@gmail.com>
2024-08-19 18:11:14 -0600

    reformat FruitItem to meet requirements

--- src/components/features/fruit-panel/FruitItem.tsx
+++ src/components/features/fruit-panel/FruitItem.tsx
@@ -3,16 +3,27 @@ import { TFruit } from "@/types";
 import { ReactNode } from "react";
 
 type TFruitItemProps = {
-    fruit: TFruit;
-    handleClick: () => void;
-    isDisabled?: boolean;
-    buttonContent: ReactNode;
-}
+  fruit: TFruit;
+  handleClick: () => void;
+  isDisabled?: boolean;
+  buttonContent: ReactNode;
+};
 
-export const FruitItem: React.FC<TFruitItemProps> = ({ fruit, handleClick, isDisabled, buttonContent }) => {
-    return <div className="flex justify-between m-2">
+export const FruitItem: React.FC<TFruitItemProps> = ({
+  fruit,
+  handleClick,
+  isDisabled,
+  buttonContent,
+}) => {
+  return (
+    <div className="flex m-2 justify-between">
+      <div className="flex w-full gap-x-2">
         <p>{fruit.name}</p>
-        <p>{fruit.nutritions.calories} kcal</p>
-        <Button disabled={isDisabled} onClick={handleClick}>{buttonContent}</Button>
+        <p>({fruit.nutritions.calories} kcal)</p>
+      </div>
+      <Button disabled={isDisabled} onClick={handleClick}>
+        {buttonContent}
+      </Button>
     </div>
-}
+  );
+};
